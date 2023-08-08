0e710e5ed8236ec487286ced2e45d69d0adc5b4d
Miguel Calixto <miguel98266@gmail.com>
2023-08-07 23:56:02 -0600

    EjemploString Concatenacion

---
+++ src/EjemploStringConcatenacion.java
@@ -0,0 +1,10 @@
+public class EjemploStringConcatenacion {
+    public static void main(String[] args) {
+        String curso = "Programacion Java";
+        String profesor = "Miguel Calixto";
+
+        String detalle = curso + " con el instructor " + profesor;
+        System.out.println(detalle);
+
+    }
+}
